8146fa1514f2d83579761424878a319ea00aa2be
unknown <yetia@yaberra.com>
2018-01-19 15:23:40 -0500

    updated model

--- models/question.js
+++ models/question.js
@@ -1,10 +1,10 @@
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
+  var Question = sequelize.define("Question", {
     username: DataTypes.STRING,
     city:DataTypes.STRING
     state:DataTypes.STRING
     avatar:DataTypes.STRING
   });
 
-  return User;
+  return Question;
 };
