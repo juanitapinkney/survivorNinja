c3d5320c3b7de51317435683aba727fbf9c562f4
Juanita Pinkney <ms.juanitapinkney@gmail.com>
2018-01-20 15:24:11 -0500

    edits made

--- server.js
+++ server.js
@@ -40,25 +40,25 @@ db.sequelize.sync({ force: true }).then(function() {
 
 
 
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+// var mysql = require("mysql");
+// var inquirer = require("inquirer");
 
-// create the connection information for the sql database
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+// // create the connection information for the sql database
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
 
-// Your username
-  user: "root",
+// // Your username
+//   user: "root",
 
-// Your password
-  password: null,
-  database: "survivorNinja_DB"
-});
+// // Your password
+//   password: null,
+//   database: "survivorNinja_DB"
+// });
 
-// connect to the mysql server and sql database
-connection.connect(function(err) {
-  if (err) throw err;
-// run the start function after the connection is made to prompt the user
-  start();
-});
+// // connect to the mysql server and sql database
+// connection.connect(function(err) {
+//   if (err) throw err;
+// // run the start function after the connection is made to prompt the user
+//   start();
+// });
