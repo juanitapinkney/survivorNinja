e72732556bb6b00756c8d6f4eba728033ac50f72
unknown <yetia@yaberra.com>
2018-01-21 09:32:47 -0500

    html and layout changes

--- models/user.js
+++ models/user.js
@@ -20,6 +20,11 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     }
+
+    score: {
+      type:
+      allowNull: false
+    }
 });
 
   return User;
